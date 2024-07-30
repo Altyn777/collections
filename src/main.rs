2cf55aa12cc07401867ec606c3597544b9adea87
Altyn777 <darzeeia@gmail.com>
2024-07-30 11:40:05 +0300

    add_employee_to_department

--- src/main.rs
+++ src/main.rs
@@ -58,10 +58,64 @@ fn convert_str(text: &str) -> String {
     result.trim_end().to_string()
 }
 
+/*
+ * Using a hash map and vectors, create a text interface to allow a user to add employee names to a
+ * department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let
+ * the user retrieve a list of all people in a department or all people in the company by department,
+ * sorted alphabetically.
+ */
+
+type Company = HashMap<String, Vec<String>>;
+
+fn get_department<'a>(company: &'a Company, department: &'a str) -> Option<&'a Vec<String>> {
+    company.get(department)
+}
+
+fn add_employee_to_department(company: &mut Company, command: &str) {
+    let mut count = 0;
+    let mut name = "";
+    let mut department = "";
+    for word in command.split_whitespace() {
+        match count {
+            0 => {
+                if word != "Add" {
+                    println!("wrong command");
+                }
+            }
+            1 => name = word,
+
+            2 => {
+                if word != "to" {
+                    println!("wrong command");
+                }
+            }
+            3 => department = word,
+            _ => {}
+        }
+        count += 1;
+    }
+    if let Some(d) = company.get_mut(department) {
+        d.push(name.to_string());
+        d.sort();
+    } else {
+        company.insert(department.to_string(), vec![name.to_string()]);
+    }
+}
+
 fn main() {
     let mut list = vec![3, 1, 5, 7, 2, 8, 4, 1, 0];
     let ListResult { median, mode } = get_median_and_mode(&mut list);
     println!("Median is {median}, Mode is {mode}");
-    let pig_latin = convert_str("hello apple chichi");
+
+    let pig_latin = convert_str("hello apple Chichi");
     println!("{:?}", pig_latin);
+
+    let mut company: Company = HashMap::new();
+
+    add_employee_to_department(&mut company, "Add Sally to Engineering");
+    add_employee_to_department(&mut company, "Add Goblin to Engineering");
+    add_employee_to_department(&mut company, "Add Amir to Sales");
+    if let Some(names) = get_department(&company, "Engineering") {
+        println!("{:?}", names);
+    };
 }
