bfe7e4d64cfe6ed1ea46f66f262777451f1de4d1
Altyn777 <darzeeia@gmail.com>
2024-07-30 10:09:14 +0300

    first try convert_str

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,41 @@ fn get_median_and_mode(list: &mut Vec<i8>) -> ListResult {
     ListResult { median, mode }
 }
 
+// Convert strings to pig latin.
+fn convert_str(text: &str) -> String {
+    let mut result = String::new();
+    for word in text.split_whitespace() {
+        let mut chars = word.chars();
+        let mut char_to_end = 'h';
+        let mut start_char: Option<char> = None;
+        if let Some(first_letter) = &chars.next() {
+            if first_letter != &'a'
+                && first_letter != &'e'
+                && first_letter != &'i'
+                && first_letter != &'o'
+                && first_letter != &'u'
+            {
+                char_to_end = *first_letter;
+            } else {
+                start_char = Some(*first_letter)
+            }
+        };
+        let corner = chars.as_str();
+        if start_char.is_some() {
+            result.push(start_char.unwrap())
+        }
+        result.push_str(corner);
+        result.push('-');
+        result.push(char_to_end);
+        result.push_str("ay ");
+    }
+    result
+}
+
 fn main() {
     let mut list = vec![3, 1, 5, 7, 2, 8, 4, 1, 0];
     let ListResult { median, mode } = get_median_and_mode(&mut list);
     println!("Median is {median}, Mode is {mode}");
+    let pig_latin = convert_str("hello apple");
+    println!("{:?}", pig_latin);
 }
